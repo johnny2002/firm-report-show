011f25c72f5d09d52a331cae90a3a3e348224151
Johnny <zhouzwzw@cn.ibm.com>
2015-02-06 14:10:38 +0800

    compile error fixed

--- firm-report-adapter-cognos/src/test/java/com/ibm/gbsc/reportadapter/cognos/ReportBeanTest.java
+++ firm-report-adapter-cognos/src/test/java/com/ibm/gbsc/reportadapter/cognos/ReportBeanTest.java
@@ -6,21 +6,18 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.testng.annotations.Test;
 
-import com.ibm.gbsc.reportadapter.cognos.CMBean;
-import com.ibm.gbsc.reportadapter.cognos.ReportBean;
-import com.ibm.gbsc.reportadapter.cognos.ReportParameter;
-
 @ContextConfiguration(locations = { "classpath:test-irmp-report-dao.xml", "classpath:test-irmp-report-res.xml" })
 @TransactionConfiguration(defaultRollback = false)
-public class ReportBeanTest extends AbstractTestNGSpringContextTests{
-	@Inject ReportBean reportBean;
-	@Inject CMBean cmBean;
-	
+public class ReportBeanTest extends AbstractTestNGSpringContextTests {
+	@Autowired
+	ReportBean reportBean;
+	@Autowired
+	CMBean cmBean;
+
 	@Test
-	public void testReportRun() throws Exception{
-		String reportPath = "/content/folder[@name='02信用风险管理']/folder[@name='0201信用风险概览']"
-				+ "/report[@name='信用风险概览']";
+	public void testReportRun() throws Exception {
+		String reportPath = "/content/folder[@name='02信用风险管理']/folder[@name='0201信用风险概览']" + "/report[@name='信用风险概览']";
 		ReportParameter[] param = new ReportParameter[0];
-		reportBean.runReportAsHTMLFrag(reportPath, param );
+		reportBean.runReportAsHTMLFrag(reportPath, param);
 	}
 }
